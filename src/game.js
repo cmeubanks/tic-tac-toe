1bfc2dca86cf2bdf55aa37bb351c8602c1559f7f
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-05 11:18:52 -0700

    completed game architecture

---
+++ src/game.js
@@ -0,0 +1,28 @@
+class Game {
+  constructor(player1, player2) {
+  this.winningCombos = []; //figure out which boxes count as wins and add here
+  this.player1 = player1;
+  this.player2 = player2;
+  this.playerTurn = playerInstance.token;
+  this.currentPlaysPlayer1 = [];
+  this.currentPlaysPlayer2 = [];
+
+  }
+
+  switchTurn() {
+    //modifies this.playerTurn
+  }
+
+  checkForWin() {
+    //loops through this.winningCombos and compairs to current player combos
+  }
+
+  drawGame() {
+    //runs through players current plays to see if they equal a certain #
+    // && do not match a winning combo
+  }
+
+  resetGame() {
+    //button to reset game?
+  }
+}
