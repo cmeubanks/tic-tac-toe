7a4a6d2a00d8a6c51643a13ac91037db835c1fbe
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-05 18:29:24 -0700

    IDs can be selected and pushed into game class partial

--- src/game.js
+++ src/game.js
@@ -1,17 +1,34 @@
 class Game {
-  constructor() {
+  constructor(player1, player2) {
   this.id = Date.now();
-  this.winningCombos = []; //figure out which boxes count as wins and add here
-  // this.player1 = player1;
-  // this.player2 = player2;
-  this.playerTurn = playerInstance.token;
-  this.currentPlaysPlayer1 = [];
-  this.currentPlaysPlayer2 = [];
+  this.winningCombos = {
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+  }; 
+  this.player1 = player1;
+  this.player2 = player2;
+  // this.playerTurn = player1.token || player2.token;
+  this.currentPlayer = player1.token;
+  this.playsByPlayer1 = [];
+  this.playsByPlayer2 = [];
+
+  }
+
+  updateGameData() {
+    if(this.player1.id)
+    this.playsByPlayer1.push(this.player1.id);
 
   }
 
   switchTurn() {
     //modifies this.playerTurn
+
   }
 
   checkForWin() {
--- src/main.js
+++ src/main.js
@@ -6,17 +6,21 @@ var gameGrid = document.querySelector('#gameGrid');
 
 
 ////////// Event Listeners /////////
-gameGrid.addEventListener('click', getSelectedBox)
+gameGrid.addEventListener('click', startGame);
 
 
 
 //game defaults to player1 turn to start
-getSelectedBox() {
-
+function startGame () {
+  trackGamePlay(event);
 }
 
-startGame() {
-  var id = getSelectedBox();
-  var player1 = new Player(id, )
-
+function trackGamePlay(event) {
+  var id = event.target.getAttribute('id');
+  var player1 = new Player(id, 'star', true)
+  var player2 = new Player(null, 'heart', false)
+  //star will later be used in an .innerHTML conditional
+  var game = new Game(player1, player2)
+  game.updateGameData();
+  console.log(game);
 }
